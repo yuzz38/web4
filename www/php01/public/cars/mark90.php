a54375849eeda37f2094365cb19561b2b5db7768
Полунин Лев <81235245+yuzz38@users.noreply.github.com>
2025-04-03 10:32:33 +0800

    Add files via upload

---
+++ www/php01/public/cars/mark90.php
@@ -0,0 +1,24 @@
+<?php
+   
+    $url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); 
+
+    $is_image = ($url == "/mark90/image");
+    $is_info = ($url == "/mark90/info");
+?>
+<h1>Марк 2 в 90 кузове</h1>
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-link <?php echo $is_image ? 'active' : ''; ?>" href="mark90/image">Картинка</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php echo $is_info ? 'active' : ''; ?>" href="mark90/info">Описание</a>
+    </li>
+</ul>
+
+<div class="border p-3 mt-3">
+    <?php  if ($is_image) {
+        require "mark90_image.php";
+    } elseif ($is_info) {
+        require "mark90_info.php";
+    } ?>
+</div>
---
+++ www/php01/public/cars/mark90_image.php
@@ -0,0 +1,2 @@
+
+<img src="../images/mark90.png" alt="mark90">
